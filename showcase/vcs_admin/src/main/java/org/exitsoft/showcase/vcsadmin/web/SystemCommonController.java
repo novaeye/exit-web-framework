2b83d60cd652e7f6848afe341c043c2775b156fc
vincent <es_vincent@163.com>
2012-12-21 21:00:56 -0800

    删除异常处理器，将shiro的未授权信息改为跳转到401.jsp页面提示

--- showcase/vcs_admin/src/main/java/org/exitsoft/showcase/vcsadmin/web/SystemCommonController.java
+++ showcase/vcs_admin/src/main/java/org/exitsoft/showcase/vcsadmin/web/SystemCommonController.java
@@ -5,7 +5,6 @@ import org.exitsoft.showcase.vcsadmin.service.account.AccountManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -69,18 +68,5 @@ public class SystemCommonController {
 		
 		return "false";
 		
-	}
-	
-	/**
-	 * 异常处理C
-	 * 
-	 * @param e Exception
-	 * 
-	 * @return String
-	 */
-	@ExceptionHandler
-	public @ResponseBody String exception(Exception e) {
-		e.printStackTrace();
-		return e.getMessage();
 	}
 }
