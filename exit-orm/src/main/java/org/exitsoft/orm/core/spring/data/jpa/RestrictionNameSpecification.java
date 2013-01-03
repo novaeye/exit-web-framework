2f06cd1585e2859f0cbfae2e0d3855eeb3068d75
exitsoft <es_vincent@163.com>
2013-01-03 12:41:29 +0800

    #24 使用jpa所有方法

--- exit-orm/src/main/java/org/exitsoft/orm/core/spring/data/jpa/RestrictionNameSpecification.java
+++ exit-orm/src/main/java/org/exitsoft/orm/core/spring/data/jpa/RestrictionNameSpecification.java
@@ -17,21 +17,33 @@ import org.springframework.data.jpa.domain.Specification;
 public class RestrictionNameSpecification<T> implements Specification<T>{
 	
 	private JpaRestrictionBuilder restrictionBuilder = new JpaRestrictionBuilder();
-	
+	//属性名称
 	private String propertyName;
+	//值
 	private Object value;
+	//约束名称
 	private String restrictionName;
 	
 	public RestrictionNameSpecification() {
 		
 	}
-	
+	/**
+	 * 构造属性名查询Specification
+	 * 
+	 * @param propertyName 对象属性名
+	 * @param value 值
+	 * @param restrictionName 约束名称
+	 */
 	public RestrictionNameSpecification(String propertyName, Object value,String restrictionName) {
 		this.propertyName = propertyName;
 		this.value = value;
 		this.restrictionName = restrictionName;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see org.springframework.data.jpa.domain.Specification#toPredicate(javax.persistence.criteria.Root, javax.persistence.criteria.CriteriaQuery, javax.persistence.criteria.CriteriaBuilder)
+	 */
 	public Predicate toPredicate(Root<T> root, CriteriaQuery<?> query,CriteriaBuilder builder) {
 		
 		restrictionBuilder.setSpecificationProperty(root,query,builder);
