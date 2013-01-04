bb2f7455527d587e7740f393c4bf66a6db97bd0a
exitsoft <es_vincent@163.com>
2013-01-04 10:40:07 +0800

    修改批处理文件,将jpa演示例子也可作为项目模板

--- archetype-generate.bat
+++ archetype-generate.bat
@@ -6,7 +6,15 @@ if not exist %~dp0\generated-sources (md %~dp0\generated-sources)
 
 cd %~dp0\generated-sources
 
-mvn archetype:generate -DarchetypeGroupId=org.exitsoft.showcase -DarchetypeArtifactId=exitsoft-vcs-admin-archetype -DarchetypeVersion=1.0.0
+
+:message
+set /p var=1:vcs-admin-panel 2:vcs-admin-jpa-panel Ñ¡ÔñÄ£°å:
+
+if "%var%" == "1" (
+call mvn archetype:generate -DarchetypeGroupId=org.exitsoft.showcase -DarchetypeArtifactId=exitsoft-vcs-admin-archetype -DarchetypeVersion=1.1.0
+) else if "%var%" == "2" (
+call mvn archetype:generate -DarchetypeGroupId=org.exitsoft.showcase -DarchetypeArtifactId=exitsoft-vcs-admin-jpa-archetype -DarchetypeVersion=1.1.0
+) else ( goto message )
 
 cd %~dp0
 
--- install.bat
+++ install.bat
@@ -1,16 +1,31 @@
 @echo off
+
+set base_path=%cd%
+
 echo [INFO] install jar to local m2 repository.
 
 call mvn clean source:jar install -Dmaven.test.skip=true
 
 echo [INFO] create vcs admin archetype
 
-cd showcase\vcs_admin
+cd %base_path%\showcase\vcs_admin
 call mvn archetype:create-from-project
 
-cd target\generated-sources\archetype
+cd %base_path%\showcase\vcs_admin\target\generated-sources\archetype
 call mvn clean install -Dmaven.test.skip=true
 
-cd %~dp0
+cd %base_path%\showcase\vcs_admin_jpa
+call mvn archetype:create-from-project
+
+cd %base_path%\showcase\vcs_admin_jpa\target\generated-sources\archetype
+call mvn clean install -Dmaven.test.skip=true
+
+cd %base_path%\showcase\vcs_admin
+rd /S /Q target
+
+cd %base_path%\showcase\vcs_admin_jpa
+rd /S /Q target
+
+cd %base_path%
 
 pause
